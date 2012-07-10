1c0a68a8c8d39ef51f2a7875030a6608796eac28
Robert Meijers <robert.meijers@gmail.com>
2012-07-10 21:32:26 +0200

    player: properly check if playerId matches on announcement
    Not all messages have a player value, which results in a value(playerid).toInt()
    of 0 which is the same as AudioPlayer.

--- xbmc/player.cpp
+++ xbmc/player.cpp
@@ -202,7 +202,7 @@ QString Player::state() const
 void Player::receivedAnnouncement(const QVariantMap &map)
 {
     QVariantMap data= map.value("params").toMap().value("data").toMap();
-    if(data.value("player").toMap().value("playerid").toInt() != playerId()) {
+    if(!data.value("player").isValid() || data.value("player").toMap().value("playerid").toInt() != playerId()) {
         return;
     }
 
