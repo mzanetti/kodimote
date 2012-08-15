90ae54ea9e6dcd55e8a972c120f809ac3cbcfe8b
Michael Zanetti <michael_zanetti@gmx.net>
2012-08-15 21:45:52 +0200

    added turkish translation files

--- messages.sh
+++ messages.sh
@@ -3,5 +3,6 @@ lupdate xbmcremote.pro -ts i18n/xbmcremote_nl.ts -no-obsolete
 lupdate xbmcremote.pro -ts i18n/xbmcremote_pt.ts -no-obsolete
 lupdate xbmcremote.pro -ts i18n/xbmcremote_it.ts -no-obsolete
 lupdate xbmcremote.pro -ts i18n/xbmcremote_el.ts -no-obsolete
+lupdate xbmcremote.pro -ts i18n/xbmcremote_tr.ts -no-obsolete
 
 lrelease i18n/xbmcremote_*.ts
